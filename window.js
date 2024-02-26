a4bfabae43a51ef9c49cb57ae4d33c81ab01013d
Matheus Iago <120859355+theus-iago@users.noreply.github.com>
2024-02-26 20:13:56 -0300

    update window.js

--- window.js
+++ window.js
@@ -1,129 +1,168 @@
 class Janela {
-  static content_header = "Titulo";
-  static content_main = "exemplo";
-  static content_footer = null;
-  static cor = "gray";
+  static header_content = "Titulo";
+  static main_content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid similique fuga rerum excepturi, amet, quibusdam laudantium praesentium iure aspernatur. Doloremque amet pariatur eius dicta accusantium id consequuntur quas sequi rerum.";
+  static footer_content = null;
+  static color = "#1e1e1e";
   static destino = document.body;
-  static divmsg = null;
-  
+  static area_msg = null;
+  static type = null;
+
+  // types: confirm, prompt, alert;
+
   static config(config) {
-    this.content_header = config.header;
-    this.content_main = config.main;
-    this.cor = config.cor;
-    this.destino =  document.body;
+    this.header_content = config.header_content;
+    this.main_content = config.main_content;
+    this.color = config.color;
+    this.type = config.type;
+
+    if (config.header_content == undefined) {
+      this.header_content = "Titulo";
+    }
+    if (config.main_content == undefined) {
+      this.main_content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid similique fuga rerum excepturi, amet, quibusdam laudantium praesentium iure aspernatur. Doloremque amet pariatur eius dicta accusantium id consequuntur quas sequi rerum.";
+    }
+    if (config.color == undefined) {
+      this.color = "#1e1e1e";
+    }
+    if (config.type == undefined) {
+      this.type = "alert";
+    }
   }
-  static mostrar() {
-    this.divmsg = document.createElement("div");
-    const estilo_divmsg = 
-     "display: flex;" +
-     "justify-content: center;" +
-     "align-items: center;" +
-     "flex-direction: column;" +
-     "position: absolute;" +
-     "top: 0px;" +
-     "left: 0px;" +
-     "width: 100%;" +
-     "height: 100vh;" +
-     "background-color: rgba(0,0,0,0.7);";
-     this.divmsg.setAttribute("id", "divmsg");
-     this.divmsg.setAttribute("style", estilo_divmsg);
-     this.destino.prepend(this.divmsg);
-     
-     const estilo_header = 
-     "display: flex;" +
-     "justify-content: flex-start;" +
-     "align-items: flex-start;" +
-     "flex-direction: column;" +
-     "width: 300px;";
-     
-     const header = document.createElement("div");
-     header.setAttribute("style", estilo_header);
-     this.divmsg.appendChild(header);
-     
-     const estilo_content_header = 
-     "display: flex;" +
-     "justify-content: flex-start;" +
-     "align-items: center;" +
-     "text-align: center;" +
-     "width: 100%;" +
-     "background-color:"+this.cor+";" +
-     "color: #fff;" +
-     "padding: 5px;" +
-     "border-radius: 5px 5px 0px 0px;";
-     
-     const content_header = document.createElement("div");
-     content_header.setAttribute("style", estilo_content_header);
-     content_header.innerHTML = this.content_header;
-     header.appendChild(content_header);
-     
-     const estilo_main = 
-     "display: flex;" +
-     "justify-content: flex-start;" +
-     "align-items: flex-start;" +
-     "flex-direction: column;" +
-     "width: 300px;";
-     
-     const main = document.createElement("div");
-     main.setAttribute("style", estilo_main);
-     this.divmsg.appendChild(main);
-     
-     const estilo_content_main = 
-     "display: flex;" +
-     "justify-content: center;" +
-     "align-items: center;" +
-     "text-align: center;" +
-     "width: 100%;" +
-     "background-color: #fff;" +
-     "color: #000;" +
-     "padding: 5px;";
-     
-     const content_main = document.createElement("div");
-     content_main.setAttribute("style", estilo_content_main);
-     content_main.innerHTML = this.content_main;
-     main.appendChild(content_main);
-     
-     const estilo_footer = 
-     "display: flex;" +
-     "justify-content: flex-start;" +
-     "align-items: flex-start;" +
-     "flex-direction: column;" +
-     "width: 300px;";
-     
-     const footer = document.createElement("div");
-     footer.setAttribute("style", estilo_footer);
-     this.divmsg.appendChild(footer);
-     
-     const estilo_content_footer = 
-     "display: flex;" +
-     "justify-content: center;" +
-     "align-items: center;" +
-     "text-align: center;" +
-     "width: 100%;" +
-     "background-color: #fff;" +
-     "color: #fff;" +
-     "padding: 5px;";
-     
-     const estilo_botao_footer = 
-     "background-color:"+this.cor+";" +
-     "width: 100%;" +
-     "border: none;" +
-     "border-radius: 6px;" +
-     "color: #fff;" +
-     "padding: 5px 0px 5px 0px;";
-     
-     const content_footer = document.createElement("div");
-     const botao_footer = document.createElement("button");
-     botao_footer.innerHTML = "Ok";
-     content_footer.setAttribute("style", estilo_content_footer);
-     botao_footer.setAttribute("style", estilo_botao_footer);
-     
-     content_footer.appendChild(botao_footer);
-     footer.appendChild(content_footer);
-     
-     botao_footer.addEventListener("click", this.ocultar);
+  static exibir() {
+    this.area_msg = document.createElement("div");
+    const style_area =
+    "background-color: rgba(0,0,0,0.7);" +
+    "height: 100%;" +
+    "width: 100%;" +
+    "position: absolute;" +
+    "top: 0px;" +
+    "left: 0px;" +
+    "display: flex;" +
+    "align-items: center;" +
+    "justify-content: center;" +
+    "flex-direction: column;";
+
+    this.area_msg.setAttribute("style", style_area);
+    this.destino.appendChild(this.area_msg);
+    this.destino.style.overflowY = "hidden";
+
+    const box_msg = document.createElement("div");
+    const style_box_msg =
+    "background-color: #fff;" +
+    "border-radius: 8px;" +
+    "overflow: hidden;" +
+    "width: 300px;" +
+    "display: flex;" +
+    "align-items: center;" +
+    "justify-content: center;" +
+    "flex-direction: column;";
+    box_msg.setAttribute("style", style_box_msg);
+    this.area_msg.prepend(box_msg);
+
+    const header = document.createElement("div");
+    const titulo = document.createElement("h3");
+    const style_header =
+    "background-color:"+this.color+";" +
+    "color: #fff;" +
+    "border-radius: 0px;" +
+    "width: 100%;" +
+    "display: flex;" +
+    "align-items: center;" +
+    "justify-content: center;" +
+    "flex-direction: column;";
+    titulo.innerHTML = this.header_content;
+    header.appendChild(titulo);
+    header.setAttribute("style", style_header);
+    box_msg.prepend(header);
+
+    const main = document.createElement("div");
+    const paragrafo = document.createElement("p");
+    const style_main =
+    "background-color: #fff;" +
+    "border-radius: 0px;" +
+    "width: 100%;" +
+    "padding: 5px;" +
+    "display: flex;" +
+    "align-items: center;" +
+    "justify-content: start;" +
+    "text-align: start;" +
+    "flex-direction: column;";
+    paragrafo.innerHTML = this.main_content;
+    main.appendChild(paragrafo);
+    main.setAttribute("style", style_main);
+    box_msg.appendChild(main);
+
+    const footer = document.createElement("div");
+    const style_footer =
+    "background-color: #fff;" +
+    "color: #fff;" +
+    "padding: 5px;" +
+    "border-radius: 0px;" +
+    "width: 100%;" +
+    "display: flex;" +
+    "align-items: center;" +
+    "justify-content: start;" +
+    "text-align: center;" +
+    "flex-direction: row;";
+
+    const style_botao_footer =
+    "background-color:"+this.color+";" +
+    "border: none;" +
+    "border-radius: 4px;" +
+    "color: #fff;" +
+    "padding: 5px 0px 5px 0px;" +
+    "width: 100%;" +
+    "display: flex;" +
+    "align-items: center;" +
+    "justify-content: start;" +
+    "text-align: center;" +
+    "flex-direction: column;";
+    
+    const style_botao_cancel =
+    "background-color: #c8c8c8;" +
+    "color: #1e1e1e;" +
+    "border: none;" +
+    "border-radius: 4px;" +
+    "margin: 3px;" +
+    "padding: 5px 0px 5px 0px;" +
+    "width: 100%;" +
+    "display: flex;" +
+    "align-items: center;" +
+    "justify-content: start;" +
+    "text-align: center;" +
+    "flex-direction: column;";
+
+    if (this.type == "alert") {
+      const botao_footer = document.createElement("button");
+      botao_footer.innerHTML = "OK";
+      botao_footer.setAttribute("style", style_botao_footer);
+      footer.appendChild(botao_footer);
+      botao_footer.addEventListener("click", () => {
+        this.ocultar();
+      });
+    }else if(this.type == "confirm") {
+      const botao_cancel = document.createElement("button");
+      const botao_ok = document.createElement("button");
+      botao_cancel.innerHTML = "Cancel";
+      botao_cancel.setAttribute("style", style_botao_cancel);
+      footer.appendChild(botao_cancel);
+      botao_cancel.addEventListener("click", () => {
+        this.ocultar();
+      });
+      
+      botao_ok.innerHTML = "Ok";
+      botao_ok.setAttribute("style", style_botao_footer);
+      footer.appendChild(botao_ok);
+      botao_ok.addEventListener("click", () => {
+        this.ocultar();
+      });
+    }
+    footer.setAttribute("style", style_footer);
+    box_msg.appendChild(footer);
   }
-  static ocultar = () => {
-    this.destino.removeChild(divmsg);
+  static ocultar() {
+    this.destino.style.overflowY = "";
+    this.destino.removeChild(this.area_msg);
   }
 }
 export default Janela;
